930cedb009db3ff4579f94d060ca6d8c8ab384ae
Maxime Boulanger <maximeboulanger_@hotmail.fr>
2023-03-19 15:43:01 +0100

    Add classes Car & Expense to the Models folder

---
+++ GasMonitor/Models/Car.swift
@@ -0,0 +1,34 @@
+//
+//  Car.swift
+//  GasMonitor
+//
+//  Created by Maxime BOULANGER on 19/03/2023.
+//
+
+import Foundation
+
+
+class Car: Identifiable, Decodable {
+    var id:UUID?
+    var brand:String
+    var model:String
+    var nickname:String?
+    var energy:String
+    var image:String
+    var milage:Int
+    var modelYear:Date
+    var fuelConsumption:String
+    var tankCapacity:Int?
+    var horsepower:Int?
+    var engineSize:Float?
+}
+
+class Expense: Identifiable, Decodable {
+    var id:UUID?
+    var car:[Car]
+    var liters:Float
+    var price:Float
+    var traveledDistance:Float
+    var energy:Int?
+    var place:String?
+}
