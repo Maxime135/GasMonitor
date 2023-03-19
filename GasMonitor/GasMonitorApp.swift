e96dadcc72c13ea47c2eff9eb5f9a0305d9083fd
Maxime Boulanger <maximeboulanger_@hotmail.fr>
2023-03-19 15:27:22 +0100

    Initial Commit

---
+++ GasMonitor/GasMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasMonitorApp.swift
+//  GasMonitor
+//
+//  Created by Maxime BOULANGER on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GasMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
